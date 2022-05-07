5d016ba08ef604217c7cb9828b0cb68630aecc62
abdelhak <abdelhak.org@gmail.com>
2022-05-07 18:47:41 +0200

    add correct path for images

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
-'use strict';
+
+
 var menuBtn = document.querySelector("#menubtn");
 var sidenav = document.querySelector("#side_nav");
 var menu = document.querySelector("#menu");
-menuBtn.onclick=function (){
-if(sidenav.style.right=="-250px"){
-    sidenav.style.right="0";
-    menu.src="images/close.png";
-}
-else {
-    sidenav.style.right ="-250px";
-    menu.src="images/menu.png";
-}
-}
+menuBtn.addEventListener("click", (e)=>{
+    e.preventDefault;
+    if(sidenav.style.right =="-250px"){
+        sidenav.style.right="0";
+        menu.src="images/close.png";
+    }
+    else {
+        sidenav.style.right ="-250px";
+        menu.src ="images/menu.png";
+
+    }
+    }
+    
+);
+ 
